507615f033f7ddb2b21a5e901678399d66341c2d
karlaemartins <karlaemartinss@gmail.com>
2026-04-14 17:36:48 -0300

    feat: implement save and load in FavoritesStorageService

--- Core/Services/Storage/FavoritesStorageService.swift
+++ Core/Services/Storage/FavoritesStorageService.swift
@@ -13,4 +13,27 @@ final class FavoritesStorageService {
     private let defaults = UserDefaults.standard
     private let sitesKey = "favorite_sites"
     
+    
+    func saveSites(_ sites: [Site]) {
+        do {
+               let data = try JSONEncoder().encode(sites)
+               defaults.set(data, forKey: sitesKey)
+           } catch {
+               print("Erro ao salvar sites:", error)
+           }
+    }
+    
+    func loadSites() -> [Site] {
+        guard let data = defaults.data(forKey: sitesKey) else {
+                return []
+            }
+            
+            do {
+                let sites = try JSONDecoder().decode([Site].self, from: data)
+                return sites
+            } catch {
+                print("Erro ao carregar sites:", error)
+                return []
+            }
+    }
 }
