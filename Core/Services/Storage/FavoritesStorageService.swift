a11f77ca141ccd3cc54fb454b3d9eaa59fa7ec13
karlaemartins <karlaemartinss@gmail.com>
2026-04-14 17:06:53 -0300

    feat: add storage structure and Site model

---
+++ Core/Services/Storage/FavoritesStorageService.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesStorageService.swift
+//  WebNavigator
+//
+//  Created by Karla E. Martins Fernandes on 14/04/26.
+//
+
+import Foundation
+
+
+final class FavoritesStorageService {
+    
+    private let defaults = UserDefaults.standard
+    private let sitesKey = "favorite_sites"
+    
+}
